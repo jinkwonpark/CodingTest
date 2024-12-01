1856e97d0f544b062e34656bf9f1c8268226e65a
jinkwonpark <jk3473@naver.com>
2024-12-01 23:48:18 +0900

    Q. 18108 - 1998년생인 내가 태국에서는 2541년생?!

---
+++ Java_BaekJoon/src/step01/Q18108.java
@@ -0,0 +1,15 @@
+package step01;
+
+import java.util.Scanner;
+
+public class Q18108 {
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int year1 = scanner.nextInt();
+        int year2 = year1 - 543;
+
+        System.out.println(year2);
+    }
+}
