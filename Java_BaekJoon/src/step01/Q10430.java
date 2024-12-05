9687ade5177af5631246ee0af4538a84a5949224
jinkwonpark <jk3473@naver.com>
2024-12-05 23:25:27 +0900

    [수정] Q. 10430 - 나머지

--- Java_BaekJoon/src/step01/Q10430.java
+++ Java_BaekJoon/src/step01/Q10430.java
@@ -7,9 +7,13 @@ public class Q10430 {
     public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
-            int A = scanner.nextInt();
-            int B = scanner.nextInt();
-            int C = scanner.nextInt();
+            String input = scanner.nextLine();
+
+            String values[] = input.split("\\s+");
+
+            int A = Integer.parseInt(values[0]);
+            int B = Integer.parseInt(values[1]);
+            int C = Integer.parseInt(values[2]);
 
             System.out.println((A + B) % C);
             System.out.println(((A % C) + (B % C)) % C);
